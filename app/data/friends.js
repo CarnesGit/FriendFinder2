316c7c271a50033f8816b2944872c39203570b85
Rick <rick.carnes@gmail.com>
2019-08-26 13:35:00 -0400

    Fished Friend Finder app

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [{
+        name: "Elmo",
+        photo: "https://avatars2.githubusercontent.com/u/23724909?s=460&v=4",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Big Bird",
+        photo: "https://pbs.twimg.com/profile_images/2678494952/5f461cc98a6252a2c9c32a6cfb6d9882_400x400.jpeg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Oscar",
+        photo: "https://pbs.twimg.com/media/DVYjQXQX0AAf5dF.jpg",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Bert",
+        photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-ses-bert.jpg",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Ernie",
+        photo: "http://coolspotters.com/files/photos/949763/ernie-profile.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Grover",
+        photo: "https://www.eufsd.org/cms/lib/NY01913270/Centricity/Domain/291/grover.jpg",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+];
+
+module.exports = friends;
